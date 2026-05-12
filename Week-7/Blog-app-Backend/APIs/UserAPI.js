7296bb89a6f3fadd2f305459b040d2e2ba50c915
kAlaveni21 <24eg105q44@anurag.edu.in>
2026-05-12 15:11:55 +0530

    Update UserAPI.js

--- Week-7/Blog-app-Backend/APIs/UserAPI.js
+++ Week-7/Blog-app-Backend/APIs/UserAPI.js
@@ -1,31 +1,57 @@
 import exp from 'express'
-import { ArticleModel } from '../models/ArticleModel.js'
-import { verifyToken } from '../middlewares/VerifyToken.js'
-export const userApp=exp.Router()
-userApp.get('/articles',verifyToken("USER"),async(req,res)=>{
-    //read articles
-    const articleList=await ArticleModel.find({isArticleActive:true})
-    //res
-    res.status(200).json({message:"articles",payload:articleList})
+import {ArticleModel} from '../models/ArticleModel.js'
+import {verifyToken} from '../middlewares/VerifyToken.js'
+export const userApp= exp.Router()
+
+//Read all the articles
+userApp.get("/articles", verifyToken("USER"), async (req, res) => {
+  try {
+    // read ALL articles (not just one)
+    const articleList = await ArticleModel.find({ isArticleActive: true });
+
+    res.status(200).json({
+      message: "Articles fetched successfully",
+      payload: articleList,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "Failed to fetch articles",
+    });
+  }
 })
 
 
-//add comments to an article
-userApp.put("/articles",verifyToken("USER"),async(req,res)=>{
-    //get body from req
-    const {articleId,comment}=req.body
-    //check article
-    const articleDocument=await ArticleModel.findOne({_id:articleId,isArticleActive:true})
-    //if article not found
-    if(!articleDocument){
-        return res.status(404).json({message:"article not found"})
+//Add comments to an article
+userApp.put("/articles", verifyToken("USER"), async (req, res) => {
+  try {
+    const { articleId, comment } = req.body;
+
+    const articleDocument = await ArticleModel.findOne({
+      _id: articleId,
+      isArticleActive: true,
+    });
+
+    if (!articleDocument) {
+      return res.status(404).json({ message: "Article not found" });
     }
-    //get user id
-    const userId=req.user?.id
-    //add comments to comment arry of articledocument
-    articleDocument.comments.push({user:userId,comment:comment})
-    //save
-    await articleDocument.save()
-    //send res
-    res.status(200).json({message:"commet added successfully",payload:articleDocument})
-})
+
+    const userId = req.user?.id;
+
+    // ✅ FIXED FIELD NAME
+    articleDocument.comments.push({
+      user: userId,
+      comment: comment,
+    });
+
+    await articleDocument.save();
+
+    res.status(200).json({
+      message: "Comment added successfully", // ✅ lowercase
+      payload: articleDocument,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "Failed to add comment",
+    });
+  }
+});
