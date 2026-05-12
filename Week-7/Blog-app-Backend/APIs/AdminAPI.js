6eff58ccee2682b984190ea57b488133476033b7
kAlaveni21 <24eg105q44@anurag.edu.in>
2026-05-12 15:10:18 +0530

    Update AdminAPI.js

--- Week-7/Blog-app-Backend/APIs/AdminAPI.js
+++ Week-7/Blog-app-Backend/APIs/AdminAPI.js
@@ -1,62 +1,28 @@
 import exp from 'express'
-import { UserModel } from '../models/UserModel.js'
-import { verifyToken } from '../middlewares/VerifyToken.js'
+import {UserModel} from '../models/UserModel.js'
+import {verifyToken} from '../middlewares/VerifyToken.js'
 export const adminApp=exp.Router()
-//read emails of user and author
-adminApp.get("/user/author",verifyToken("ADMIN"),async(req,res)=>{
-    //get email of user and author
-const userEmail=await UserModel.find({role:{$in:["USER","AUTHOR"]}}).select("email role -_id")
-//check user  and author email there are not
-if(userEmail.length===0)
-{
-    return res.status(404).json({message:"email not found"})
-}
-res.status(200).json({mesaage:"emails",payload:userEmail})
+
+adminApp.get("/users",verifyToken("ADMIN"),async(req,res)=>{
+    let users=await UserModel.find({role:"USER"})
+    res.status(200).json({message:"Users found successfully",payload:users})
 })
-// block or activate user or author 
-adminApp.put("/user/block/:id", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const userId = req.params.id;
 
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { isActive: false },
-      { new: true }
-    );
+adminApp.get("/authors",verifyToken("ADMIN"),async(req,res)=>{
+    let authors=await UserModel.find({role:"AUTHOR"})
+    res.status(200).json({message:"Authors found successfully",payload:authors})
+})
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+adminApp.patch("/users",verifyToken("ADMIN"),async(req,res)=>{
+    let {userId,isUserActive}=req.body
+    let user=await UserModel.findOne({_id:userId})
+    if(!user){
+        return res.status(404).json({message:"User not found"})
     }
-
-    res.status(200).json({
-      message: "User blocked successfully",
-      payload: user
-    });
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-adminApp.put("/user/activate/:id", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const userId = req.params.id;
-
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { isActive: true },
-      { new: true }
-    );
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    if(isUserActive==user.isUserActive){
+        return res.status(200).json({message:"User is already in the same state"})
     }
-
-    res.status(200).json({
-      message: "User activated successfully",
-      payload: user
-    });
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+    user.isUserActive=isUserActive
+    await user.save()
+    res.status(200).json({message:"User modified",payload:user})
+})
